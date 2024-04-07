0f9f84c1d5a74b10dc8ee974d9ab0626d896cd48
Sh4D0ws2705 <nguyenhieunghia2004@gmail.com>
2024-04-07 10:25:24 +0700

    trang sua sp

--- resources/views/orderList.blade.php
+++ resources/views/orderList.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Order List</h1>
+        <h1 class="h3 mb-0 text-gray-800">Danh Sách Hóa Đơn</h1>
     </div>
     <!-- table order -->
     <div class="card shadow mb-4">
